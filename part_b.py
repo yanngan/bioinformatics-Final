ca0128125e2826a71705c38a09284cde2cb7471c
yanngan <55285071+yanngan@users.noreply.github.com>
2022-01-22 16:20:24 +0200

    add to part B

--- part_b.py
+++ part_b.py
@@ -1,9 +1,44 @@
 import part_a
 import tools
 
+def cross_reference_GB_UniProt(UniProt_file_path, GB_file_path):
+    '''
+    Making Cross-reference between UniProt and GB
+    the Cross-reference is based on 'GOA' id
+    print a report of the Cross-reference
+    :param UniProt_file_path: the path to the UniProt file
+    :param GB_file_path: the path to the GB file
+    :return: nothing
+    '''
+    uniprot_protein_dictionary = tools.get_dictionary_of_protain_UniProt_file(UniProt_file_path)
+    gb_protein_dictionary = tools.get_dictionary_of_protain_from_GB_file(GB_file_path)
+
+    count_uniprot_in_gb = 0
+    for key, val in uniprot_protein_dictionary.items():
+        if key in gb_protein_dictionary:
+            count_uniprot_in_gb += 1
+
+    count_gb_in_uniprot = 0
+    for key, val in gb_protein_dictionary.items():
+        if key in uniprot_protein_dictionary:
+            count_gb_in_uniprot += 1
+
+    print("---------count uniprot in gb--------")
+    print("count_uniprot_in_gb = " + str(count_uniprot_in_gb))
+    print("uniprot len = " + str(len(uniprot_protein_dictionary)))
+
+    print("---------count gb in uniprot--------")
+    print("count_gb_in_uniprot = " + str(count_gb_in_uniprot))
+    print("gb len = " + str(len(gb_protein_dictionary)))
+
 
 def main_b():
-    part_a.main_a()
     print("in b")
-    tools.import_data_from_DG_file("BS168.gb")
-    tools.import_data_from_UniProt_server("uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta")
+    # ------ b.a ------
+    cross_reference_GB_UniProt("uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta", "BS168.gb")
+    # ------ b.b ------
+    print(str(tools.get_transmembrane_from_UniProt_file("uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta")))
+
+
+
+
--- tools.py
+++ tools.py
@@ -3,7 +3,7 @@ from Bio import GenBank ,SeqIO
 
 
 
-def import_data_from_DG_file( file_path):
+def import_data_from_DB_file( file_path):
     '''
     import data from local DG file
     :param file_path: the path leading to the file
@@ -25,3 +25,49 @@ def import_data_from_UniProt_server(fasta_path):
     for record in SeqIO.parse(fasta_path, "fasta"):
         list_records.append(record)
     return list_records
+
+def get_dictionary_of_protain_UniProt_file(fasta_path):
+    '''
+    :param fasta_path: the path to the fasta file
+    :return: dictionary: key = GOA identifier, value = protain seq
+    '''
+    uniprot_protein_list = import_data_from_UniProt_server(fasta_path)
+    dictionary_to_return = {}
+    for protain in uniprot_protein_list:
+        dictionary_to_return[protain.name.split("|")[1]] = protain.seq
+        if protain.name.split("|")[1] == "O32050":
+            print("hello")
+    return dictionary_to_return
+
+def get_dictionary_of_protain_from_GB_file(file_path):
+    '''
+    :param: file_path: the path to the DB file
+    :return:  dictionary: key = GOA identifier, value = protain seq
+    '''
+    debug = 0
+    input_handle = open(file_path, "r")
+    dictionary_to_return = {}
+    for seq_record in SeqIO.parse(input_handle, "genbank"):
+        print("Dealing with GenBank record %s" % seq_record.id)
+        for seq_feature in seq_record.features :
+            if seq_feature.type == "CDS":
+                assert len(seq_feature.qualifiers['translation']) == 1
+                debug += 1
+                # output_handle = open("faa_filename.txt", "a")
+                # output_handle.write(">%s from %s\n%s\n" % (
+                #     str(seq_feature),#.qualifiers['locus_tag'][0],
+                #     seq_record.name,
+                #     seq_feature.qualifiers['translation'][0]))
+                if any("GOA" in s for s in seq_feature.qualifiers['db_xref']):
+                    dictionary_to_return[seq_feature.qualifiers['db_xref'][2].split(":")[1]] = seq_feature.qualifiers['translation'][0]
+    print("len of full CDS DB records = " + str(debug))
+    print("len of CDS DB records having in 'db_xref' the identifier 'GOA' = " + str(len(dictionary_to_return)))
+    return dictionary_to_return
+
+def get_transmembrane_from_UniProt_file(fasta_path):
+    uniprot_protein_list = import_data_from_UniProt_server(fasta_path)
+    dictionary_to_return = {}
+    for protain in uniprot_protein_list:
+        if protain.description.find("transmembrane") != -1:
+            dictionary_to_return[protain.name.split("|")[1]] = protain.seq
+    return dictionary_to_return
