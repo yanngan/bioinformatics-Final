79116782b362d71d64b161860ff619b292e3c605
yanngan <55285071+yanngan@users.noreply.github.com>
2022-01-25 18:01:02 +0200

    working on part 2

--- part_b.py
+++ part_b.py
@@ -10,34 +10,110 @@ def cross_reference_GB_UniProt(UniProt_file_path, GB_file_path):
     :param GB_file_path: the path to the GB file
     :return: nothing
     '''
-    uniprot_protein_dictionary = tools.get_dictionary_of_protain_UniProt_file(UniProt_file_path)
-    gb_protein_dictionary = tools.get_dictionary_of_protain_from_GB_file(GB_file_path)
+    GOA_uniprot_protein_dictionary, Name_uniprot_protein_dictionary = tools.get_dictionary_of_protain_UniProt_file(UniProt_file_path)
+    GOA_gb_protein_dictionary, Name_gb_protein_dictionary= tools.get_dictionary_of_protain_from_GB_file(GB_file_path)
 
-    count_uniprot_in_gb = 0
-    for key, val in uniprot_protein_dictionary.items():
-        if key in gb_protein_dictionary:
-            count_uniprot_in_gb += 1
+    count_uniprot_in_gb_by_GOA = 0
+    for key, val in GOA_uniprot_protein_dictionary.items():
+        if key in GOA_gb_protein_dictionary:
+            count_uniprot_in_gb_by_GOA += 1
 
-    count_gb_in_uniprot = 0
-    for key, val in gb_protein_dictionary.items():
-        if key in uniprot_protein_dictionary:
-            count_gb_in_uniprot += 1
+    count_uniprot_in_uniprot_by_Name = 0
+    for key, val in Name_uniprot_protein_dictionary.items():
+        if key in Name_gb_protein_dictionary:
+            count_uniprot_in_uniprot_by_Name += 1
+
+    count_gb_in_uniprot_by_GOA = 0
+    for key, val in GOA_gb_protein_dictionary.items():
+        if key in GOA_uniprot_protein_dictionary:
+            count_gb_in_uniprot_by_GOA += 1
+
+    count_gb_in_uniprot_by_Name = 0
+    for key, val in Name_gb_protein_dictionary.items():
+        if key in Name_uniprot_protein_dictionary:
+            count_gb_in_uniprot_by_Name += 1
 
     print("---------count uniprot in gb--------")
-    print("count_uniprot_in_gb = " + str(count_uniprot_in_gb))
-    print("uniprot len = " + str(len(uniprot_protein_dictionary)))
+    print("count_uniprot_in_gb by GOA = " + str(count_uniprot_in_gb_by_GOA))
+    print("count_uniprot_in_gb by Name = " + str(count_uniprot_in_uniprot_by_Name))
+    print("uniprot GOA len = " + str(len(GOA_uniprot_protein_dictionary)))
+    print("uniprot Name len = " + str(len(Name_uniprot_protein_dictionary)))
 
     print("---------count gb in uniprot--------")
-    print("count_gb_in_uniprot = " + str(count_gb_in_uniprot))
-    print("gb len = " + str(len(gb_protein_dictionary)))
+    print("count_gb_in_uniprot by GOA = " + str(count_gb_in_uniprot_by_GOA))
+    print("count_gb_in_uniprot by Name = " + str(count_gb_in_uniprot_by_Name))
+    print("gb GOA len = " + str(len(GOA_gb_protein_dictionary)))
+    print("gb Name len = " + str(len(Name_gb_protein_dictionary)))
+
+
+
+
+
+
+def research_transmembrane(UniProt_file_path):
+    _,name_dictionary_to_return = tools.get_transmembrane_from_UniProt_file(UniProt_file_path)
+    list_len = []
+    for key,val in name_dictionary_to_return.items():
+        list_len.append(len(val))
+    all_values = name_dictionary_to_return.values()
+    print("---------------------------")
+    print("max length of protein containing transmembrane part = " + str(max(list_len)))
+    print("min length of protein containing transmembrane part = " + str(min(list_len)))
+    print("AVG length of protein containing transmembrane part = " + str(sum(list_len) / len(list_len)))
+
+    tools.create_histogram(list_len, "Histogram - length of protein containing Transmembrane part")
+
+    map_distribution = {}
+    for key,val in name_dictionary_to_return.items():
+        count_hydrophobic = 0
+        for char in val:
+            if char in tools.dictionary_Hydrophobic_amino_acids:
+                count_hydrophobic += 1
+        map_distribution[key] = count_hydrophobic/len(val)
+    print("Distribution of hydrophobic amino acids for each one of the transmembrane protein : (GOA,Distribution)")
+    print(str(map_distribution))
+    print("total avg of hydrophobic amino acids in those transmembrane protein = " + str(sum(map_distribution.values())/len(name_dictionary_to_return)))
+
+
+
+def research_GC(UniProt_file_path,gb_file_path):
+    _, dictionary_of_transmembrane = tools.get_transmembrane_from_UniProt_file(UniProt_file_path)
+    _, A_Name_gb_protein_dictionary = tools.get_dictionary_of_protain_from_GB_file(gb_file_path)
+
+    B = {}
+    global_len = 0
+    GC_count = 0
+    for key, val in dictionary_of_transmembrane.items():
+        if key in A_Name_gb_protein_dictionary:
+            B[key] = val
+            global_len += len(val)
+            GC_count += val.count('G')
+            GC_count += val.count('C')
+
+    print("GC Distribution in B group is  = " + str((GC_count/global_len)*100) +"%")
+
+
+
+
+
+
+
+
 
 
 def main_b():
     print("in b")
+
+    uniprot_file_path = "uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta"
+    gb_file_path = "BS168.gb"
+
     # ------ b.a ------
-    cross_reference_GB_UniProt("uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta", "BS168.gb")
+    cross_reference_GB_UniProt(uniprot_file_path, gb_file_path )
     # ------ b.b ------
-    print(str(tools.get_transmembrane_from_UniProt_file("uniprot-organism Bacillus+subtilis+(strain+168)+[224308] .fasta")))
+    research_transmembrane(uniprot_file_path)
+    # ------ b.b ------
+    research_GC(uniprot_file_path,gb_file_path)
+
 
 
 
--- tools.py
+++ tools.py
@@ -1,5 +1,12 @@
 
 from Bio import GenBank ,SeqIO
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# ---------- const ------------------
+dictionary_Hydrophobic_amino_acids = {"A", "P", "L", "I", "V", "M", "P", "T"}
+
 
 
 
@@ -32,42 +39,56 @@ def get_dictionary_of_protain_UniProt_file(fasta_path):
     :return: dictionary: key = GOA identifier, value = protain seq
     '''
     uniprot_protein_list = import_data_from_UniProt_server(fasta_path)
-    dictionary_to_return = {}
+    GOA_dictionary_to_return = {}
+    Name_dictionary_to_return = {}
     for protain in uniprot_protein_list:
-        dictionary_to_return[protain.name.split("|")[1]] = protain.seq
-        if protain.name.split("|")[1] == "O32050":
-            print("hello")
-    return dictionary_to_return
+        GOA_dictionary_to_return[protain.name.split("|")[1]] = protain.seq
+        for val in protain.description.split():
+            if "GN=" in val:
+                Name_dictionary_to_return[val[3:]] = protain.seq
+    return GOA_dictionary_to_return,Name_dictionary_to_return
 
 def get_dictionary_of_protain_from_GB_file(file_path):
     '''
     :param: file_path: the path to the DB file
     :return:  dictionary: key = GOA identifier, value = protain seq
     '''
-    debug = 0
+    count_entry = 0
     input_handle = open(file_path, "r")
-    dictionary_to_return = {}
+    GOA_dictionary_to_return = {}
+    Name_dictionary_to_return = {}
     for seq_record in SeqIO.parse(input_handle, "genbank"):
         print("Dealing with GenBank record %s" % seq_record.id)
         for seq_feature in seq_record.features :
             if seq_feature.type == "CDS":
                 assert len(seq_feature.qualifiers['translation']) == 1
-                debug += 1
-                # output_handle = open("faa_filename.txt", "a")
-                # output_handle.write(">%s from %s\n%s\n" % (
-                #     str(seq_feature),#.qualifiers['locus_tag'][0],
-                #     seq_record.name,
-                #     seq_feature.qualifiers['translation'][0]))
+                count_entry += 1
                 if any("GOA" in s for s in seq_feature.qualifiers['db_xref']):
-                    dictionary_to_return[seq_feature.qualifiers['db_xref'][2].split(":")[1]] = seq_feature.qualifiers['translation'][0]
-    print("len of full CDS DB records = " + str(debug))
-    print("len of CDS DB records having in 'db_xref' the identifier 'GOA' = " + str(len(dictionary_to_return)))
-    return dictionary_to_return
+                    GOA_dictionary_to_return[seq_feature.qualifiers['db_xref'][2].split(":")[1]] = seq_feature.qualifiers['translation'][0]
+                if seq_feature.qualifiers.get('gene') != None:
+                    Name_dictionary_to_return[seq_feature.qualifiers.get('gene')[0]] = seq_feature.qualifiers['translation'][0]
+    print("len of full CDS GB records = " + str(count_entry))
+    return GOA_dictionary_to_return,Name_dictionary_to_return
 
 def get_transmembrane_from_UniProt_file(fasta_path):
     uniprot_protein_list = import_data_from_UniProt_server(fasta_path)
-    dictionary_to_return = {}
+    GOA_dictionary_to_return = {}
+    Name_dictionary_to_return = {}
     for protain in uniprot_protein_list:
         if protain.description.find("transmembrane") != -1:
-            dictionary_to_return[protain.name.split("|")[1]] = protain.seq
-    return dictionary_to_return
+            GOA_dictionary_to_return[protain.name.split("|")[1]] = protain.seq
+            for val in protain.description.split():
+                if "GN=" in val:
+                    Name_dictionary_to_return[val[3:]] = protain.seq
+    return GOA_dictionary_to_return,Name_dictionary_to_return
+
+
+def create_histogram(val_list, title="histogram"):
+    n, bins, patches = plt.hist(x=val_list, bins='auto', color='#0504aa')
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Value')
+    plt.ylabel('Frequency')
+    plt.title(title)
+    plt.show()
+
+
